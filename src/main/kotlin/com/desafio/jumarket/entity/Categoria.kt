dc693e263fdc1551d6016a8d8b28faeda7825754
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2023-07-09 16:06:01 -0400

    Adicionando e configurando o banco de dados

--- src/main/kotlin/com/desafio/jumarket/entity/Categoria.kt
+++ src/main/kotlin/com/desafio/jumarket/entity/Categoria.kt
@@ -1,7 +1,17 @@
 package com.desafio.jumarket.entity
 
+import jakarta.persistence.*
+@Entity
+@Table(name = "Categoria")
 data class Categoria(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long,
+
+        @Column(nullable = false)
         val nome: String = "",
+
+        @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE],
+                mappedBy = "categoria")
         var produto: List<Produto> = mutableListOf()
 )
--- src/main/kotlin/com/desafio/jumarket/entity/Produto.kt
+++ src/main/kotlin/com/desafio/jumarket/entity/Produto.kt
@@ -1,9 +1,23 @@
 package com.desafio.jumarket.entity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Produto")
 data class Produto(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long,
+
+        @Column(nullable = false)
         val nome: String = "",
-        val medida: String = "",
+
+        @Column(nullable = false)
+        val unidade_Medida: String = "",
+
+        @Column(nullable = false)
         var preco: Double,
+
+        @ManyToOne
         val categoria: Categoria
 )
